864ec37c160c5c8569e91013aa729c5cca697746
hadi chloun <hadi_960@hotmail.com>
2022-01-28 08:17:30 -0600

    adds scaffold/pins

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_26_202304) do
+ActiveRecord::Schema.define(version: 2022_01_28_025813) do
+
+  create_table "pins", force: :cascade do |t|
+    t.string "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
