ad69a44f1dff4dc86d93cce56c9be44471ccbb5f
hadi chloun <hadi_960@hotmail.com>
2022-02-01 23:16:09 -0600

    adds activerecord and shows description and add image from local pc

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,3 +1,5 @@
 class Pin < ApplicationRecord
     belongs_to :user 
+    has_one_attached :profile_image #, styles: { medium: "300x300>", thumb: "100x100>" }
+    #validates_attachment :image, content_type: { content_type: ["image/jpg", "image/jpeg", "image/png", "image/gif"] }
 end
